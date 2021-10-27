3405703fe1e415f11ebea558a4fded5b71f100e8
Danny Tuppeny <danny@tuppeny.com>
2021-10-27 15:42:17 +0100

    Basic repro

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-repro-dc2569" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-repro-dc2569.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-repro-dc2569!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+		{ scheme: 'file' },
+		new CompletionProvider(),
+		'(',
+	));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class CompletionProvider implements vscode.CompletionItemProvider {
+	public async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): Promise<vscode.CompletionItem[]> {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		const item = new vscode.CompletionItem("barbarbarbarbar", vscode.CompletionItemKind.Variable);
+		item.detail = "bool";
+		item.sortText = "aaaaaa";
+		item.textEdit = { range: new vscode.Range(position, position), newText: "barbarbarbarbar" };
+
+		return [item];
+	}
+
+	public async resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken): Promise<vscode.CompletionItem> {
+		await new Promise((resolve) => setTimeout(resolve, 100));
+		return item;
+	}
+}
